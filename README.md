281a34bb13f894ac513fb3b03c6b742a4f139fc8
kyle calica <kyle.calica-steinhilber@workday.com>
2016-11-18 14:25:43 -0800

    tested storing links and displaying them in json

---
+++ README.md
@@ -0,0 +1,1 @@
+Must run mongod first in one terminal and run node in another to get everything to work
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,57 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+//input details into system /share
+router.post('/share', function(req, res, next){
+	
+	//switch to be inputed as parameters of the request
+	//save share 
+	var share = new Shares({ title: 'Example', link: 'server1.com'});
+
+	//save to database 
+	share.save(function(err){
+		if(err){
+			console.log(err);
+		} else {
+			
+			console.log(share);
+			res.send(share);  
+
+		}
+	});	
+	
+});
+
+
+
+//get all fluffs 
+router.get('/fluffs',function(req, res, next){
+		
+	Shares.find(function(err, shares){
+		if(err) return console.error(err); 
+
+		console.log(shares);
+		 
+
+	});
+	
+	
+});
+router.get('/test',function(req, res, next){
+	
+	Shares.find(function(err,shares){
+		if(err) return console.error(err);
+			
+		res.render('../views/link', { shares: shares });
+		
+	});
+		
+});
+//generate share link/page /fluff/{id}
+	//router render page with metadata dynamically inserted 
+
+
+//check stats of fluff link /stats/{id}
+	//grab the proper fluff 
 module.exports = router;
